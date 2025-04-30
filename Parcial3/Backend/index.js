ec09f3f0655f3bf1248683c3bb659410d6c9580e
Juan Jaime Gutierrez <m1lksk1ttles@outlook.com>
2025-04-30 07:00:23 -0600

    NO REQUESTEA 30/05/24

--- Parcial3/Backend/index.js
+++ Parcial3/Backend/index.js
@@ -3,14 +3,31 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+app.use(express.text());
+
 app.get('/guitarras', (req,res) => {
-    res.send('Hello World!');
+    //Parametro de Cadena de Consulta
+    console.log(req.query)
+    console.log(req.query.id)
+    res.send('Get. Server is running on port 3000');
 });
 
 app.post('/guitarras', (req,res) => {
-    res.send('Hello World!');
+    console.log(req.body);
+    res.send('Post. Server is running on port 3000');
 });
 
-app.listen(3000, () =>{
-    console.log('Server is running on port 3000');
+app.patch('/guitarras', (req,res) => {
+    console.log(req.params);
+    res.send('Patch. Server is running on port 3000');
+});
+
+app.delete('/guitarras', (req,res) => {
+    console.log(req.params);
+    res.send('Delete. Server is running on port 3000');
+});
+
+app.listen(3005, () =>{
+    console.log('Listen. Server is running on port 3000');
 });
